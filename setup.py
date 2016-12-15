3364f537cae3b2a1ed9e9dfa6dae036eadfff060
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-12-15 20:06:19 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django-model-to-dict',
-    version='0.1.0',
+    version='0.2.0',
     description="""Django Model to Python dict serialization mixin.""",
     long_description=readme + '\n\n' + history,
     author='Grigoriy Beziuk',
